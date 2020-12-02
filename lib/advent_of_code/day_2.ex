730f09f1aedfca487665aafda8eaea32d31d51c0
Patryk Woziński <patryk.wozinski@gmail.com>
2020-12-02 16:17:05 +0100

    cleanup

--- lib/advent_of_code/day_2.ex
+++ lib/advent_of_code/day_2.ex
@@ -4,14 +4,15 @@ defmodule AdventOfCode.Day2 do
   def calculate(file_path) do
     file_path
     |> File.stream!()
-    |> Stream.map(&(String.replace(&1, "\n", "")))
+    |> Stream.map(&String.replace(&1, "\n", ""))
     |> Stream.map(fn line ->
       Regex.named_captures(@pattern, line)
     end)
     |> Stream.filter(fn %{"char" => char, "min" => min, "max" => max, "password" => password} ->
-      occurrences = password
-      |> String.graphemes()
-      |> Enum.count(&(&1 == char))
+      occurrences =
+        password
+        |> String.graphemes()
+        |> Enum.count(&(&1 == char))
 
       occurrences >= String.to_integer(min) and occurrences <= String.to_integer(max)
     end)

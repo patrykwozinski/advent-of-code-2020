898caf74ed0f5dc1078905a3140b35fbf7624b6c
Patryk Woziński <patryk.wozinski@gmail.com>
2020-12-02 16:21:45 +0100

    cleanup for part 2

--- test/advent_of_code/day2_test.exs
+++ test/advent_of_code/day2_test.exs
@@ -3,11 +3,13 @@ defmodule AdventOfCodeTest.Day2Test do
 
   alias AdventOfCode.Day2
 
-  test "should be calculated how many passwords is valid" do
-    assert Day2.calculate("test/fixtures/day2/short.txt") == 2
-  end
+  describe "Part 1 of the task" do
+    test "should be calculated how many passwords is valid" do
+      assert Day2.calculate("test/fixtures/day2/short.txt") == 2
+    end
 
-  test "day 2 solution" do
-    assert Day2.calculate("test/fixtures/day2/full.txt") == 546
+    test "day 2 solution" do
+      assert Day2.calculate("test/fixtures/day2/full.txt") == 546
+    end
   end
 end

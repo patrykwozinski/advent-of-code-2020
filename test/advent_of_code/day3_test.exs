13962165e3c54b807ab964559ef94c2e51cea76e
Patryk Woziński <patryk.wozinski@gmail.com>
2020-12-03 09:08:10 +0100

    the answer for full

--- test/advent_of_code/day3_test.exs
+++ test/advent_of_code/day3_test.exs
@@ -4,8 +4,12 @@ defmodule AdventOfCodeTest.Day3Test do
   alias AdventOfCode.Day3Part1
 
   describe "Part 1 of the task" do
-    test "the toboggan should met 7 threes" do
+    test "the toboggan should meet 7 trees" do
       assert Day3Part1.calculate("test/fixtures/day3/short.txt") == 7
+    end
+
+    test "the toboggan should meet x trees" do
+      assert Day3Part1.calculate("test/fixtures/day3/full.txt") == 274
     end
   end
 end

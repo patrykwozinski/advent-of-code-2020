4b6f1fc983cf48fb64a80666d67516e37c5439a2
Patryk Woziński <patryk.wozinski@gmail.com>
2020-12-04 10:19:52 +0100

    brute force

--- lib/advent_of_code/day4_part1.ex
+++ lib/advent_of_code/day4_part1.ex
@@ -8,25 +8,67 @@ defmodule AdventOfCode.Day4Part1 do
       |> Enum.flat_map(&String.split(&1))
     end)
     # |> Enum.filter(&(is_required(&1)))
-    |> Enum.filter(&(all_required_fields?(&1)))
+    |> Enum.filter(&all_required_fields?(&1))
     |> Enum.count()
   end
 
   defp all_required_fields?(document) do
+    has_byr =
+      Enum.any?(document, fn data ->
+        case data do
+          "byr" <> _ -> true
+          _ -> false
+        end
+      end)
 
-  end
+    has_iyr =
+      Enum.any?(document, fn data ->
+        case data do
+          "iyr" <> _ -> true
+          _ -> false
+        end
+      end)
 
-  defp is_required(data) do
-    case data do
-      "byr" <> _ -> true
-      "iyr" <> _ -> true
-      "eyr" <> _ -> true
-      "hgt" <> _ -> true
-      "hcl" <> _ -> true
-      "ecl" <> _ -> true
-      "pid" <> _ -> true
-      "cid" <> _ -> true
-      _ -> false
-    end
+    has_eyr =
+      Enum.any?(document, fn data ->
+        case data do
+          "eyr" <> _ -> true
+          _ -> false
+        end
+      end)
+
+    has_hgt =
+      Enum.any?(document, fn data ->
+        case data do
+          "hgt" <> _ -> true
+          _ -> false
+        end
+      end)
+
+    has_hcl =
+      Enum.any?(document, fn data ->
+        case data do
+          "hcl" <> _ -> true
+          _ -> false
+        end
+      end)
+
+    has_ecl =
+      Enum.any?(document, fn data ->
+        case data do
+          "ecl" <> _ -> true
+          _ -> false
+        end
+      end)
+
+    has_pid =
+      Enum.any?(document, fn data ->
+        case data do
+          "pid" <> _ -> true
+          _ -> false
+        end
+      end)
+
+    has_byr and has_iyr and has_eyr and has_hgt and has_hcl and has_ecl and has_pid
   end
 end

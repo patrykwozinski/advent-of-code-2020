d635a53a9f64adb6c806117523d5f7cb559225bb
Patryk Woziński <patryk.wozinski@gmail.com>
2020-12-05 13:22:19 +0100

    maybe that's good way?

--- lib/advent_of_code/day5.ex
+++ lib/advent_of_code/day5.ex
@@ -1,5 +1,34 @@
 defmodule AdventOfCode.Day5 do
   def part1(file_path) do
+    file_path
+    |> File.stream!()
+    |> Stream.map(&String.replace(&1, "\n", ""))
+    |> Stream.map(fn bp ->
+      bp
+      |> String.graphemes()
+      |> seat_id({0, 127}, {0, 7})
+      |> IO.inspect()
+    end)
+    |> Enum.max()
+  end
 
+  defp seat_id(["F" | rest], {row_start, row_end}, column) do
+    seat_id(rest, {row_start, row_end}, column)
+  end
+
+  defp seat_id(["B" | rest], {row_start, row_end}, column) do
+    seat_id(rest, {row_start, row_end}, column)
+  end
+
+  defp seat_id(["L" | rest], row, {column_start, column_end}) do
+    seat_id(rest, row, {column_start, column_end})
+  end
+
+  defp seat_id(["R" | rest], row, {column_start, column_end}) do
+    seat_id(rest, row, {column_start, column_end})
+  end
+
+  defp seat_id([], {row, _}, {column, _}) do
+    {row, column}
   end
 end
--- test/advent_of_code/day5_test.exs
+++ test/advent_of_code/day5_test.exs
@@ -5,7 +5,7 @@ defmodule AdventOfCodeTest.Day5Test do
 
   describe "Part 1 of the boarding" do
     test "short list highest seat ID is 820" do
-      assert Day4Part1.calculate("test/fixtures/day5/short.txt") == 820
+      assert Day5.part1("test/fixtures/day5/short.txt") == 820
     end
   end
 end

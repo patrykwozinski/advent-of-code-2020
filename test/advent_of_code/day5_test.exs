17c2ee76f04ea85ca6a380248540e7910a373115
Patryk Woziński <patryk.wozinski@gmail.com>
2020-12-05 14:00:24 +0100

    first step done

--- test/advent_of_code/day5_test.exs
+++ test/advent_of_code/day5_test.exs
@@ -7,5 +7,9 @@ defmodule AdventOfCodeTest.Day5Test do
     test "short list highest seat ID is 820" do
       assert Day5.part1("test/fixtures/day5/short.txt") == 820
     end
+
+    test "full list highest seat ID is 989" do
+      assert Day5.part1("test/fixtures/day5/full.txt") == 989
+    end
   end
 end

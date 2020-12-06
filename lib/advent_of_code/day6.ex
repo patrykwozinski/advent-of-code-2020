a60c36ef4ba11d4aa9bfea73eacc7c5ba6c96488
Patryk Woziński <patryk.wozinski@gmail.com>
2020-12-06 11:08:03 +0100

    close to done

--- lib/advent_of_code/day6.ex
+++ lib/advent_of_code/day6.ex
@@ -1,7 +1,25 @@
 defmodule AdventOfCode.Day6 do
   def part1(file_path) do
+    file_path
+    |> read_groups()
+    |> Enum.map(fn group ->
+      group
+      |> Enum.flat_map(& &1)
+      |> Enum.uniq()
+    end)
   end
 
   def part2(file_path) do
+  end
+
+  defp read_groups(file_path) do
+    file_path
+    |> File.stream!()
+    |> Stream.chunk_by(&(&1 == "\n"))
+    |> Stream.filter(&(&1 != ["\n"]))
+    |> Stream.map(fn group ->
+      Enum.map(group, &(String.replace(&1, "\n", "") |> String.graphemes()))
+    end)
+    |> Enum.to_list()
   end
 end

e6d445832d5443eec50d4ad4c1f8ae36bbb872e5
Patryk Woziński <patryk.wozinski@gmail.com>
2020-12-06 10:41:51 +0100

    6 day init

---
+++ lib/advent_of_code/day6.ex
@@ -0,0 +1,4 @@
+defmodule AdventOfCode.Day6 do
+  def part1(file_path) do
+  end
+end
---
+++ test/advent_of_code/day6_test.exs
@@ -0,0 +1,15 @@
+defmodule AdventOfCodeTest.Day6Test do
+  use ExUnit.Case
+
+  alias AdventOfCode.Day6
+
+  describe "Part 1 of the custom customs" do
+    test "short list contain different 11 answers" do
+      assert Day6.part1("test/fixtures/day6/short.txt") == 11
+    end
+
+    test "full list contain different x answers" do
+      assert Day6.part1("test/fixtures/day6/full.txt") == 0
+    end
+  end
+end

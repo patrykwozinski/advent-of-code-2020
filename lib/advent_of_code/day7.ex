12e4bf6e3960ca16bfa884caef9859d478fef412
Patryk Woziński <patryk.wozinski@gmail.com>
2020-12-07 11:27:05 +0100

    invert list

--- lib/advent_of_code/day7.ex
+++ lib/advent_of_code/day7.ex
@@ -4,6 +4,7 @@ defmodule AdventOfCode.Day7 do
   def part1(file_path) do
     file_path
     |> read_lines()
+    |> invert_list()
   end
 
   def part2(_file_path) do
@@ -21,7 +22,6 @@ defmodule AdventOfCode.Day7 do
     |> Map.new(fn [bag, contained] ->
       {bag, prepare_contained(contained)}
     end)
-    |> IO.inspect()
   end
 
   defp prepare_contained(contained) when is_binary(contained) do
@@ -38,6 +38,14 @@ defmodule AdventOfCode.Day7 do
       [no_bags, bag] = Regex.run(@bags, bags, capture: :all_but_first)
 
       {String.to_integer(no_bags), bag}
+    end)
+  end
+
+  defp invert_list(list) do
+    Enum.reduce(list, %{}, fn {parent, children}, inverted_tree ->
+      Enum.reduce(children, inverted_tree, fn {_n, child}, inverted_tree ->
+        Map.update(inverted_tree, child, [parent], &[parent | &1])
+      end)
     end)
   end
 end

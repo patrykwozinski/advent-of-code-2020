e4dc7f9cd3ac341c4d21516b480345b360b22ad1
Patryk Woziński <patryk.wozinski@gmail.com>
2020-12-07 09:09:16 +0100

    init test

--- test/advent_of_code/day7_text.exs
+++ test/advent_of_code/day7_text.exs
@@ -2,4 +2,14 @@ defmodule AdventOfCodeTest.Day7Test do
   use ExUnit.Case
 
   alias AdventOfCode.Day7
+
+  describe "Part 1 of the bags check" do
+    test "shiny gold bag could might be in 4 bags in short list" do
+      assert Day6.part1("test/fixtures/day7/short.txt") == 4
+    end
+
+    # test "shiny gold bag could might be in X bags in full list" do
+    #   assert Day6.part1("test/fixtures/day7/full.txt") == 0
+    # end
+  end
 end

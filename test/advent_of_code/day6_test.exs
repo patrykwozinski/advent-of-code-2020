3941d3456e7bed24b4e812460f4732a9e91d0284
Patryk Woziński <patryk.wozinski@gmail.com>
2020-12-07 09:07:47 +0100

    uncomment

--- test/advent_of_code/day6_test.exs
+++ test/advent_of_code/day6_test.exs
@@ -8,18 +8,18 @@ defmodule AdventOfCodeTest.Day6Test do
       assert Day6.part1("test/fixtures/day6/short.txt") == 11
     end
 
-    # test "full list contain different 6662 answers" do
-    #   assert Day6.part1("test/fixtures/day6/full.txt") == 6662
-    # end
+    test "full list contain different 6662 answers" do
+      assert Day6.part1("test/fixtures/day6/full.txt") == 6662
+    end
   end
 
-  # describe "Part 2 of the custom customs" do
-  #   test "short list contain 6 same answers in group" do
-  #     assert Day6.part2("test/fixtures/day6/short.txt") == 6
-  #   end
+  describe "Part 2 of the custom customs" do
+    test "short list contain 6 same answers in group" do
+      assert Day6.part2("test/fixtures/day6/short.txt") == 6
+    end
 
-  #   test "full list contain x same answers in group" do
-  #     assert Day6.part2("test/fixtures/day6/full.txt") == 3382
-  #   end
-  # end
+    test "full list contain x same answers in group" do
+      assert Day6.part2("test/fixtures/day6/full.txt") == 3382
+    end
+  end
 end

25bfacee62e5038f442e03340a42ad5a9dd92c87
Patryk Woziński <patryk.wozinski@gmail.com>
2020-12-07 10:10:38 +0100

    split

--- lib/advent_of_code/day7.ex
+++ lib/advent_of_code/day7.ex
@@ -1,7 +1,20 @@
 defmodule AdventOfCode.Day7 do
-  def part1(_file_path) do
+  def part1(file_path) do
+    lines = read_lines(file_path)
+    |> IO.inspect()
   end
 
   def part2(_file_path) do
+  end
+
+  defp read_lines(file_path) do
+    file_path
+    |> File.stream!()
+    |> Stream.map(fn line ->
+      line
+      |> String.replace("\n", "")
+      |> String.split(" bags contain ")
+    end)
+    |> Enum.to_list()
   end
 end

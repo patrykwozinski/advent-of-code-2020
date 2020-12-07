456fc1f2efa8de77a0df9fc57519335eca2f024e
Patryk Woziński <patryk.wozinski@gmail.com>
2020-12-07 09:24:34 +0100

    fix

--- test/advent_of_code/day7_test.exs
+++ test/advent_of_code/day7_test.exs
@@ -5,7 +5,7 @@ defmodule AdventOfCodeTest.Day7Test do
 
   describe "Part 1 of the bags check" do
     test "shiny gold bag could might be in 4 bags in short list" do
-      assert Day6.part1("test/fixtures/day7/short.txt") == 4
+      assert Day7.part1("test/fixtures/day7/short.txt") == 4
     end
 
     # test "shiny gold bag could might be in X bags in full list" do
